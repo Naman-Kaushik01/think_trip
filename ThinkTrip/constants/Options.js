ab66a715386df164e64779ce75965e802e053ffe
Naman Kaushik <kaushiknaman68@gmail.com>
2025-04-21 01:07:35 +0530

    const

---
+++ ThinkTrip/constants/Options.js
@@ -0,0 +1,30 @@
+export const SelectTravelLists=[
+    {
+        id:1,
+        title:'Just Me',
+        desc:'A solo travel in exploration',
+        icon:'',
+        people:'1'
+    },
+    {
+        id:2,
+        title:'Just Me',
+        desc:'A solo travel in exploration',
+        icon:'',
+        people:'1'
+    },
+    {
+        id:3,
+        title:'Just Me',
+        desc:'A solo travel in exploration',
+        icon:'',
+        people:'1'
+    },
+    {
+        id:4,
+        title:'Just Me',
+        desc:'A solo travel in exploration',
+        icon:'',
+        people:'1'
+    },
+]
